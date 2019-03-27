27512e4cb65e4ce1d3128f782357967ae3ef8104
jvegar <jvegar@uni.pe>
2019-03-27 11:40:22 -0500

    initial commit

---
+++ IService1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/ScheduleEventAsync",
+                  Method = "POST",
+                   RequestFormat = WebMessageFormat.Json,
+                   ResponseFormat = WebMessageFormat.Json)]
+        Task<String> ScheduleEventAsync(Event e);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Event
+    {
+        //string subject, string startTime, string endTime, string attendeeEmail
+        [DataMember]
+        public string subject { get; set; }
+        [DataMember]
+        public string startTime { get; set; }
+        [DataMember]
+        public string endTime { get; set; }
+        [DataMember]
+        public string organizerEmail { get; set; }
+        [DataMember]
+        public string attendeeEmail { get; set; }        
+        [DataMember]
+        public string bodyContent { get; set; }
+        [DataMember]
+        public string locationName { get; set; }
+        [DataMember]
+        public string categoryName { get; set; }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Helpers;
+using Calendar;
+using System.Threading.Tasks;
+
+namespace CalendarService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private static GraphServiceClient graphClient;
+        private static CalendarController cal;
+        public async Task<String> ScheduleEventAsync(Event e)
+        {            
+            graphClient = GraphServiceClientProvider.GetAuthenticatedClient();
+            cal = new CalendarController(graphClient);
+            Microsoft.Graph.Event newevent = await cal.ScheduleEventAsync(e.subject, e.startTime, e.endTime, e.organizerEmail, e.attendeeEmail, e.bodyContent, e.locationName, e.categoryName);
+            if (newevent != null) return "OK";
+            else return "Error";    
+        }
+    }
+}
